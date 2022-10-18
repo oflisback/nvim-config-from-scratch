de4105ef4a8f27dde9a81378c755cef4f39d2dc0
Ola Flisbäck <ola.flisback@gmail.com>
2022-10-18 23:01:39 +0200

    Add lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -138,6 +138,9 @@ return packer.startup(function(use)
   -- Bufferline
   use "akinsho/bufferline.nvim"
 
+  -- lualine
+  use 'nvim-lualine/lualine.nvim'
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
