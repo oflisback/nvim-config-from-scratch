4facd43357134a9cb12bfe2c52a7d032a64c2c8a
Ola Flisbäck <x-ola.flisback@modelon.com>
2023-01-15 11:59:47 +0100

    Neovide tweak

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
   vim.cmd([[
     let g:neovide_scale_factor = 1.0
+    let g:neovide_scroll_animation_length = 1.0
     let g:neovide_cursor_vfx_mode = "railgun"
     let g:neovide_refresh_rate=60
   ]])
